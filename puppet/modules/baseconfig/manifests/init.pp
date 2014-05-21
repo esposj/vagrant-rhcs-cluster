9b960c67c51f8bcee9e48ab4004fd4a291405294
Joe Esposito <joe@j-espo.com>
2014-05-20 21:09:25 -0400

    added a basic hosts file allowing access to san,server01,server02 and a floating ip

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -3,4 +3,36 @@ class baseconfig {
     package { "vim-enhanced":
         ensure => present,
     }
+    package { "parted":
+        ensure => present,
+    }
+    file { "/root/.ssh" : 
+        ensure  => 'directory',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0700',
+    }
+    file { "/root/.ssh/id_rsa" :
+        ensure  => present,
+        recurse => true,
+        replace => false,
+        source  => "/tmp/vagrant-puppet-3/modules-0/baseconfig/files/id_rsa",
+        require => File['/root/.ssh'],
+        owner   => root,
+        group   => root,
+        mode    => 0600,
+    }
+    file { "/etc/hosts" :
+        ensure  => present,
+        replace => true,
+        source  => "/tmp/vagrant-puppet-3/modules-0/baseconfig/files/hosts",
+        owner   => root,
+        group   => root,
+        mode    => 0755,
+    }
+    service { "iptables" :
+	ensure => stopped,
+	enable => false,
+    }
+
 }
